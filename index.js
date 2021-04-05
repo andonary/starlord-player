5a65db28f6210bb19f4cbdc7cc7aac16d74c75e6
Ando Nary <ando.nary@gmail.com>
2021-04-05 20:21:26 +0200

    add some songs

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const Player = require('./domain/startlord-player/mp3player');
 const Playlist = require('./domain/cassette/youtube');
 const Song = require('./domain/models/song');
 
-const song = new Song();
-song.author = 'peter';
-song.name = 'quill';
-song.path = 'oula';
+const songOne = new Song('https://www.youtube.com/watch?v=BA4rSO-h9Io', 'Come and get your love', 'Redbone');
+const songTwo = new Song('https://www.youtube.com/watch?v=-cXrEPNvRO8', 'Spirit in the sky', 'Norman Greenbaum');
+const songThree = new Song('https://www.youtube.com/watch?v=_EBvXpjudf8', 'Cherry Bomb', 'The runaways');
 
 const playlist = new Playlist();
-playlist.faceA = [song, new Song('t', 'r', 'g')]
+playlist.faceA = [songOne, songTwo, songThree];
 const player = new Player(playlist.faceA);
 
 player.play();
-player.pause();
+player.next();
+player.play();
 player.next();
 player.play();
 player.previous();
-player.play();
+player.pause();
